12f0bdd61422aa044d832bc828d86d69fe2616dd
kapil <kpkapilrana@gmail.com>
2024-03-01 21:19:48 +0530

    episode 4

--- App.js
+++ App.js
@@ -1,31 +1,320 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX HTML like - XML like Syntax (transpiled before it reaches the JS Engine) Parcel - Babel
+/* 
+  * Header
+    *Nav Items
+      Home
+      About Us
+      Services
+      Contact Us
+  * Body
+  * Footer
+  *   Copyright
+  *   Link
+  *   Contact
+*/
 
-//JSX => Babel transpiled it to React.creatElement => ReactElement-JS Object => HTMLElement(render)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// React Element
-const Title = (
-  <h1 className="head" tabIndex="5">
-    Namaste React 🚀
-  </h1>
-);
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+const RestaurantCard = (prop) => {
+  const { resData } = prop;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    sla: { deliveryTime },
+  } = resData?.info;
+  // console.log(resData);
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-image"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} Star</h4>
+      <h4>{deliveryTime} minutes</h4>
+    </div>
+  );
+};
 
-// React Component
-// Class Based Component - OLD
-// Functional Component - NEW
+const resList = [
+  {
+    info: {
+      id: "430768",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "mwakaqib8te8rp4bjb3d",
+      locality: "Byculla",
+      areaName: "CIDCO",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.5,
+      parentId: "2456",
+      avgRatingString: "4.5",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 35,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-01 02:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹299",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/dominos-pizza-byculla-cidco-aurangabad-430768",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "622541",
+      name: "Chotemiya Kitchen",
+      cloudinaryImageId: "32ae5cf541734d5f34be359d5813f183",
+      locality: "Collector Office Road",
+      areaName: "Times Colony",
+      costForTwo: "₹300 for two",
+      cuisines: ["Arabian", "Fast Food", "Lebanese"],
+      avgRating: 4.4,
+      parentId: "370518",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 6.5,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "6.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-01 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chotemiya-kitchen-collector-office-road-times-colony-aurangabad-622541",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "106044",
+      name: "CFC Shalimar Family Restaurant",
+      cloudinaryImageId: "agwwamnhrofy1ysn6nz0",
+      locality: "Trimurti Housing Society",
+      areaName: "Jaswantpura",
+      costForTwo: "₹250 for two",
+      cuisines: ["North Indian", "Chinese", "Biryani", "Mughlai", "Lucknowi"],
+      avgRating: 4.2,
+      parentId: "56435",
+      avgRatingString: "4.2",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 6.7,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "6.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-01 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/cfc-shalimar-family-restaurant-trimurti-housing-society-jaswantpura-aurangabad-106044",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "134690",
+      name: "Shawarma King Adil Feel In Saudi",
+      cloudinaryImageId: "ibfzlqv99mgfbfogfafc",
+      locality: "City Chowk",
+      areaName: "Gulmandi",
+      costForTwo: "₹250 for two",
+      cuisines: ["Lebanese", "rolls"],
+      avgRating: 4.3,
+      parentId: "182431",
+      avgRatingString: "4.3",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 7.9,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "7.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-03-01 02:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/shawarma-king-adil-feel-in-saudi-city-chowk-gulmandi-aurangabad-134690",
+      type: "WEBLINK",
+    },
+  },
+];
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="rest-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// React Functional Component
-
-const HeadingComponent = () => (
-  <div id="container">
-    {/* <Title /> */}
-    {Title}
-    <h1>Namaste React 🚀 Functional Component</h1>
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
